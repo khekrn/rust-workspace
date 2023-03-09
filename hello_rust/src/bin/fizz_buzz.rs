87bee4a202bef6854b4c91a4129422a7c54f3fa7
khekrn <kishorenayark@gmail.com>
2023-03-09 22:31:03 +0530

    Restructure is complete and now it looks good

---
+++ hello_rust/src/bin/fizz_buzz.rs
@@ -0,0 +1,22 @@
+fn main(){
+    println!("FizzBuzz program \n");
+    for i in 1..25{
+        fizzbuzz(i);
+    }
+}
+
+fn is_divisible(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false;
+    }
+    return lhs % rhs == 0;
+}
+
+fn fizzbuzz(n: u32){
+    match (is_divisible(n, 3), is_divisible(n, 5)) {
+       (true, true) => println!("fizzbuzz"),
+       (true, false) => println!("fizz"),
+       (false, true) => println!("buzz"),
+       (false, false) => println!("{n}"),  
+    }
+}
