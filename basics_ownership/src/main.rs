9bd7a30e548439e323a0c4b126834a8549b05d00
KishoreKarunakaran <75828462+khekrncs@users.noreply.github.com>
2023-03-15 21:37:45 +0530

    Adding Ownership Code

--- basics_ownership/src/main.rs
+++ basics_ownership/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    memory_layout();
+}
+
+fn memory_layout(){
+    let mut s1 = String::from("Hello");
+    s1.push(' ');
+    s1.push_str("World");
+
+    unsafe {
+        let (cap, ptr, len): (usize, usize, usize) = std::mem::transmute(s1);
+        println!("ptr = {ptr:#x}, len = {len}, capacity = {cap}");
+    }
 }
