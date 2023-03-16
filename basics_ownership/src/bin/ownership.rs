eba547c6d1657386dea76aa4d88350622fecbf69
khekrn <kishorenayark@gmail.com>
2023-03-16 20:53:49 +0530

    Completing the Ownership part

--- basics_ownership/src/bin/ownership.rs
+++ basics_ownership/src/bin/ownership.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Clone, Copy, Debug)]
 struct Point{
     x: i32,
@@ -18,6 +20,11 @@ fn main(){
     let y = String::from("Go");
     let result = max(x.as_str(), y.as_str());
     println!("Result = {result:?}");
+
+    life_time_struct();
+
+    let res = generic_max_with_announcement(&x, &y, "Learning Lifetimes");
+    println!("Result = {res:?}");
 }
 
 fn mv_semantics(){
@@ -44,10 +51,32 @@ fn max<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
-// fn lifetime_1<'a>(x: &'a str, y: &'a str) -> &'a str{
-//     if x.len() > y.len() {
-//         x
-//     }else{
-//         y
-//     }
-// }
+fn generic_max_with_announcement<'a, T>(x: &'a str, y: &'a str, announcement: T) -> &'a str 
+where T: Display{
+    println!("Attention Please : {}", announcement);
+    if x.len() > y.len() {
+        x
+    }else{
+        y
+    }
+}
+
+struct ImportantExcerpt<'a>{
+    part: &'a str,
+}
+
+impl <'a> ImportantExcerpt<'a>{
+    fn return_part(&self, announcement: &str) -> &str{
+        println!("Attention Please : {}", announcement);
+        self.part
+    }
+}
+
+fn life_time_struct(){
+    let novel = String::from("Call me Kishore. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Cannot Find");
+    let instance = ImportantExcerpt{
+        part: first_sentence,
+    };
+    println!("Result = {:?}", instance.return_part("I have a static lifetime"));
+}
